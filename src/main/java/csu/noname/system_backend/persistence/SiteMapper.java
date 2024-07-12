f97110e8a8a76da345273e409f766250edbc0e80
pincou <1044144854@qq.com>
2024-07-12 23:32:26 +0800

    Service层的部分完善

---
+++ src/main/java/csu/noname/system_backend/persistence/SiteMapper.java
@@ -0,0 +1,7 @@
+package csu.noname.system_backend.persistence;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import csu.noname.system_backend.entity.Site;
+
+public interface SiteMapper extends BaseMapper<Site> {
+}
---
+++ src/main/java/csu/noname/system_backend/persistence/StuCourMapper.java
@@ -0,0 +1,7 @@
+package csu.noname.system_backend.persistence;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import csu.noname.system_backend.entity.StuCour;
+
+public interface StuCourMapper extends BaseMapper<StuCour> {
+}
